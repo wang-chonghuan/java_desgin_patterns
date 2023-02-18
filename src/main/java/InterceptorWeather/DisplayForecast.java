2925a864ac0818b4f3af265591b999211d9b76e0
wang-chonghuan <wangchonghuan@gmail.com>
2023-02-18 12:46:03 +0000

    weather station for interceptor

---
+++ src/main/java/InterceptorWeather/DisplayForecast.java
@@ -0,0 +1,30 @@
+package InterceptorWeather;
+
+public class DisplayForecast implements Observer {
+    private float currentPressure = 29.92f;
+    private float lastPressure;
+    private Subject weatherData;
+
+    public DisplayForecast(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        lastPressure = currentPressure;
+        currentPressure = pressure;
+
+        display();
+    }
+
+    public void display() {
+        System.out.print("Forecast: ");
+        if (currentPressure > lastPressure) {
+            System.out.println("Improving weather on the way!");
+        } else if (currentPressure == lastPressure) {
+            System.out.println("More of the same");
+        } else if (currentPressure < lastPressure) {
+            System.out.println("Watch out for cooler, rainy weather");
+        }
+    }
+}
